ea0571103727ecc6f5246b8408c73acc4840b892
shogokuratomi <kurasho117@gmail.com>
2018-10-08 23:00:05 +0900

    先生ご指摘箇所を修正&9回裏まで繰り返しとした。

--- baseball_game.py
+++ baseball_game.py
@@ -5,12 +5,16 @@ class Pitcher:
         self._controll = controll
         self._speed = speed
         self._flactu = flactu
+                # 配列は辞めました。初期化メソッド呼び出し時にすべての入力値を格納しておきたいので
+                #getterは使わないことにしました。
 
     def throw(self):
         return self._controll + random.randint(-1*self._flactu, self._flactu)
+                #前verではGameクラス内にありましたが、Pitcher Classに移動しました。
 
     def pitch_power(self):
         return self._speed + random.randint(-1*self._flactu, self._flactu)
+                #_speedを使っていなかったので、HOME RUN判定に使うことにしました。
  
 class Batter:
     def __init__(self, meet, power, flactu):
@@ -23,16 +27,15 @@ class Batter:
 
     def batt_power(self):
         return self._power * 10 + random.randint(-1*self._flactu, self._flactu) * 100
+                #_powerを使っていなかったので、HOME RUN判定に使うことにしました。
 
 class Game:
     def __init__(self, pitcher, batter):
         print('ゲーム開始')
-        print('Enterを押すごとに１球投げます')
-        input()
         self._out = 0
         q = 'None'
         innings = 1
-        while (q != 'q') & ( innings < 19 ):
+        while (q != 'q') & ( innings < 19 ):     #9回裏まで繰り返しにしました。点数はありませんが・・・
             print(self.get_innings(innings))
             self._out = 0
             self.start()
@@ -48,9 +51,9 @@ class Game:
 
     def get_innings(self, innings):
         if (innings % 2) != 0:
-            return '{}回{}'.format((innings+1)//2, '表')
+            return '{}回{}'.format((innings+1)//2, '表')      #奇数なら表
         else:
-            return '{}回{}'.format((innings+1)//2, '裏')
+            return '{}回{}'.format((innings+1)//2, '裏')      #偶数なら裏
         
     def battle_main(self, pitcher, batter):
         while self.change_judgement():
@@ -67,7 +70,7 @@ class Game:
             self.start()
     
     def count(self):
-        ballcount   = 'ball  : {}'.format(str('*')*self._ball)
+        ballcount   = 'ball  : {}'.format(str('*')*self._ball)      #formatメソッドで見やすく
         strikecount = 'strike: {}'.format(str('*')*self._strike)
         outcount    = 'out   : {}'.format(str('*')*self._out)
         count = '\n'.join([ballcount, strikecount, outcount])
@@ -92,7 +95,7 @@ class Game:
     
     def hantei(self, pitcher, batter):
         if pitcher.throw() <= batter.swing():
-            if pitcher.pitch_power() <= batter.batt_power():
+            if pitcher.pitch_power() <= batter.batt_power():        #たまにHOME RUNが出ます。
                 print('          @@@@@ HOME RUN !!! @@@@@')
             else:
                 print('          Hit!')
